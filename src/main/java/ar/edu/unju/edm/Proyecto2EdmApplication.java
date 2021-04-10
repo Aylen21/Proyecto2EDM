73c2ed9361af0750043bc8eb5ce288d63ca2b5fe
aylen00 <aylen00@aylen00-desktop>
2021-04-10 01:40:05 -0300

    Inicializacion del Proyecto

---
+++ src/main/java/ar/edu/unju/edm/Proyecto2EdmApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2EdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2EdmApplication.class, args);
+	}
+
+}
