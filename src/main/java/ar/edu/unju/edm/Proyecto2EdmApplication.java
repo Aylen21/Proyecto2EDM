963e15710a9e3e2e60ae65793e3e0eac70677a22
aylen00 <aylen00@aylen00-desktop>
2021-04-10 09:44:46 -0300

    Inicializacion del Proyecto Resuelta la suma

--- src/main/java/ar/edu/unju/edm/Proyecto2EdmApplication.java
+++ src/main/java/ar/edu/unju/edm/Proyecto2EdmApplication.java
@@ -3,11 +3,16 @@ package ar.edu.unju.edm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ar.edu.unju.edm.controler.CalculadoraControler;
+
 @SpringBootApplication
 public class Proyecto2EdmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Proyecto2EdmApplication.class, args);
+		CalculadoraControler operacion = new CalculadoraControler();
+		operacion.resolverOperaciones();
 	}
 
+	
 }
---
+++ src/main/java/ar/edu/unju/edm/controler/CalculadoraControler.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.edm.controler;
+
+import ar.edu.unju.edm.model.Calculadora;
+
+public class CalculadoraControler {
+	public void resolverOperaciones() {
+		
+	int num1;
+	int num2;
+	num1 = 45;
+	num2 = 20;
+	
+	Calculadora nuevaCalculadora = new Calculadora ();
+	nuevaCalculadora.setA(num1);
+	nuevaCalculadora.setB(num2);
+
+	System.out.println("El resultdo de la suma: " + nuevaCalculadora.sumarDosnumeros() );
+	}
+
+}
